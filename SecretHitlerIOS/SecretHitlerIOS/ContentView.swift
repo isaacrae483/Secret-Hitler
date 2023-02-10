5b16d4abb9c9141cebce9abdd68d43fdf64a880d
Isaac Rae <80138639+isaacrae483@users.noreply.github.com>
2023-02-09 21:10:40 -0700

    init 2

---
+++ SecretHitlerIOS/SecretHitlerIOS/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SecretHitlerIOS
+//
+//  Created by Isaac Rae on 2/9/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            JoinView()
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SecretHitlerIOS/SecretHitlerIOS/SecretHitlerIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecretHitlerIOSApp.swift
+//  SecretHitlerIOS
+//
+//  Created by Isaac Rae on 2/9/23.
+//
+
+import SwiftUI
+
+@main
+struct SecretHitlerIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
